8b7189f65b386cd6589299287791b1f2a2e3000a
Lukas Karberg <deaderlp@googlemail.com>
2024-01-04 10:58:01 +0100

    set up axios and states

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,14 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export const PageWelcome = () => {
-	return (
-		<p>This is the welcome page.</p>
-	)
-}
+	const [appName, setAppName] = useState(`Test`);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`https://localhost:4899`);
+		})();
+	});
+
+	return <p>This is the welcome page.</p>;
+};
